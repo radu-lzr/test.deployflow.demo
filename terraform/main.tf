0d39c2e4d4349cf7ee9e4c55d2fd90a3951c56b9
Radu Lazar <radu.lazar@cellenza.com>
2026-04-14 14:07:49 +0200

    chore: add main.tf [DeployFlow]

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
-
-  # Computed attributes (read-only):
-  # location   = (computed)
-  # managed_by = (computed)
-  # tags       = (computed)
 }
