0928b03087be6bc8c1a5f447beff5fdda9e6a16a
Radu Lazar <radu.lazar@cellenza.com>
2026-04-14 14:02:20 +0200

    chore: add monitoring/main.tf [DeployFlow onboarding]

---
+++ monitoring/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "azurerm" {} # configured by Terrateam init -backend-config args
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
